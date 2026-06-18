03b49f45a1c34add7e17e45fac5d0a503dc03041
Mahé Bechler <mahe.bechler@gmail.com>
2026-06-18 17:51:37 +0200

    ajout de la soustraction/comparaison et de la division (incomplet)X

--- binary.ml
+++ binary.ml
@@ -121,6 +121,17 @@ let rec addp x y c=
        let pqr= addpb q r
        in XC (pqr, s);;    
 
+let b2b x=
+  match x with
+  | Zero -> BZ
+  | One -> BP(XH);;
+
+let addbin x y c =
+  match (x,y) with
+  | BZ, BZ -> b2b c 
+  | BZ, BP b -> BP b 
+  | BP a, BZ -> BP a 
+  | BP a, BP b -> BP (addp a b c) ;;
 
  let t n m f=
     (n2i (p2n (addp (n2p (i2n n)) (n2p (i2n m)) Zero)));;
@@ -139,6 +150,120 @@ addp (XC(XC (XC (XH, Zero), Zero), Zero)) (XC(XC(XH, Zero), Zero)) Zero ;;
 addp (XC(XC(XH, One), One)) (XC(XC(XC(XH, Zero),One),Zero)) Zero;;
 
 addp (XC(XC (XC (XH, Zero), Zero), Zero)) (XC (XH, One)) Zero ;;
+
+let multb a b=
+  match a with
+  | Zero -> Zero
+  | One -> b ;;
+  
+let rec multp x y=
+  match x with
+  | XC(p,Zero) -> XC ( multp p y, Zero)
+  | XC(p, One) -> (addp y (XC(multp p y,Zero)) Zero) 
+  | XH -> y ;;
+
+let multbin x y=
+  match (x,y) with 
+  | _,BZ -> BZ
+  | BZ,_ -> BZ
+  | BP(m),BP(n) -> BP (multp m n) ;;
+
+let test x y=
+  p2i (multp (i2p x) (i2p y));;
+  
+type diff = Neg : pos -> diff | Eq | Pos : pos -> diff
+
+
+let d2i x= 
+  match x with 
+  | Neg d -> - (p2i d)
+  | Eq -> 0
+  | Pos d -> p2i d ;;
+
+(*
+let rec predp x=
+  match x with
+  | XH -> None
+  | XC(d, One) -> Some (XC(d, Zero))
+  | XC(d, Zero) -> 
+    match predp d with
+    | None   -> Some XH
+    | Some p -> Some (XC (p,One));;
+    *)
+    
+let rec predp x=
+  match x with
+  | XC(d, One)   -> XC(d, Zero)
+  | XC(XH, Zero) -> XH
+  | XC(d,Zero)   -> XC (predp d,One);;
+
+let t x = p2i (predp (i2p x));;
+
+let diffb a b =
+  match (a, b) with
+  | One, Zero -> Pos XH
+  | Zero, One -> Neg XH
+  | _, _ -> Eq;;
+
+let rec diffp x y=
+  match (x,y) with
+  | XH, XH -> Eq
+  | _, XH  -> Pos (predp x)
+  |XH, _   -> Neg (predp y)
+  | XC(p, a), XC(q, b) ->
+    match diffp p q with
+    | Neg d -> Neg (match diffb b a with
+      | Neg _ -> predp (XC (d,Zero))
+      | Eq    -> XC (d,Zero)
+      | Pos _ -> XC (d,One)) 
+    | Eq    -> diffb a b;
+    | Pos d -> Pos (match diffb a b with
+      | Neg _ -> predp (XC (d,Zero))
+      | Eq    -> XC (d,Zero)
+      | Pos _ -> XC (d,One));; 
+
+let diffbin x y= 
+  match (x,y) with 
+  | BZ, BZ -> Eq
+  | BZ, BP a -> Neg a
+  | BP a, BZ -> Pos a
+  | BP a, BP b -> diffp a b ;;
+  
+
+let t x y= diffp (i2p x) (i2p y);;
+
+t 3 4;;
+
+let mult2b b = 
+  match b with
+  | BZ -> BZ
+  | BP p -> BP (XC (p,Zero));; 
+
+let div x d=
+  match x with
+  |XH -> (match d with 
+         | XH -> (BP XH, BZ)
+         | _  -> (BZ, BP XH))
+  |XC(p, Zero) ->
+         let (q,r) = div p d in
+         let r'= mult2b r
+         in (match diffbin r' (BP d) with
+            | Neg _ -> (mult2b q, r') 
+            | Eq    -> (addbin (mult2b q) BZ One, BZ)
+            | Pos k   -> (addbin (mult2b q) BZ One, BP k))
+  |XC(p, One) ->
+         let (q,r) = div p d in
+         let r'= addbin (mult2b r) BZ One
+         in match diffbin r' (BP d) with
+            | Neg _   -> (mult2b q, r') 
+            | Eq      -> (addbin (mult2b q) BZ One, BZ)
+            | Pos k   -> (addbin (mult2b q) BZ One, BP k) ;;
+
+let t x d = div (i2p x) (i2p d) ;;
+
+
+
+
 
 
 
